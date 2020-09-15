0900b7ada547cd41c5af03534295eee0b26fc1c1
Pavel Chernyshev <titan100695@yandex.ru>
2020-09-15 06:53:31 +0300

    emoji reader created project

---
+++ EmojiReader/EmojiReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiReader
+//
+//  Created by Павел Чернышев on 15.09.2020.
+//  Copyright © 2020 Павел Чернышев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
