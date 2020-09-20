f8535d821f99e1bc8f0e7dc8dbd0e93aa7fc61c6
Pavel Chernyshev <titan100695@yandex.ru>
2020-09-20 20:48:50 +0300

    Share Photos Action

---
+++ PhotoTaps/PhotoTaps/PhoteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhoteViewController.swift
+//  PhotoTaps
+//
+//  Created by Павел Чернышев on 20.09.2020.
+//  Copyright © 2020 Павел Чернышев. All rights reserved.
+//
+
+import UIKit
+
+class PhoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
