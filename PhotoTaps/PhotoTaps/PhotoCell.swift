ad69b1002f6d9aa5305954333425b45adba080a8
Pavel Chernyshev <titan100695@yandex.ru>
2020-09-20 07:20:34 +0300

    Collection Veiw Congiguration

---
+++ PhotoTaps/PhotoTaps/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  PhotoTaps
+//
+//  Created by Павел Чернышев on 20.09.2020.
+//  Copyright © 2020 Павел Чернышев. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
